a6135316bdf715230ed5a4475b19dd1258828d6a
이재민 <rkwoals524@hanmail.net>
2024-10-14 22:09:16 +0900

    redis connection test

--- queue/src/main/java/com/study/queue/QueueApplication.java
+++ queue/src/main/java/com/study/queue/QueueApplication.java
@@ -1,13 +1,24 @@
 package com.study.queue;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.context.ApplicationListener;
+import org.springframework.data.redis.core.ReactiveRedisTemplate;
 
+@RequiredArgsConstructor
 @SpringBootApplication
-public class QueueApplication {
+public class QueueApplication implements ApplicationListener<ApplicationReadyEvent> {
+
+    private final ReactiveRedisTemplate<String, String> reactiveRedisTemplate;
 
     public static void main(String[] args) {
         SpringApplication.run(QueueApplication.class, args);
     }
 
+    @Override
+    public void onApplicationEvent(ApplicationReadyEvent event) {
+        reactiveRedisTemplate.opsForValue().set("key", "test").subscribe();
+    }
 }
